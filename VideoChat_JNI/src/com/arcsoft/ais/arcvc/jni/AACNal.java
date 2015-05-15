6d1b850f4201688f1b3dfe9ec355790f1f670820
xudonggithub <baobaoc@126.com>
2015-05-15 10:38:15 +0800

    add jni

---
+++ VideoChat_JNI/src/com/arcsoft/ais/arcvc/jni/AACNal.java
@@ -0,0 +1,23 @@
+package com.arcsoft.ais.arcvc.jni;
+
+public class AACNal {
+	private int payloadLength;
+	private byte[] payload;
+
+	public int getPayloadLength() {
+		return payloadLength;
+	}
+
+	public void setPayloadLength(int payloadLength) {
+		this.payloadLength = payloadLength;
+	}
+
+	public byte[] getPayload() {
+		return payload;
+	}
+
+	public void setPayload(byte[] payload) {
+		this.payload = payload;
+	}
+
+}
